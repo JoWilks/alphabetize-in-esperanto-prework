8f433fab5c521f8d16edae6ca4f7257ccd93f19c
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-12 08:35:54 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,9 @@
 def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
 
-  first_order = arr.sort_by {|a| esp_alphabet.index(a[0]) 
-      esp_alphabet.index(a[1])
-  }
+  first_order = arr.sort_by {|a| esp_alphabet.index(a[0]) }
+  
+  if first_order #has any phrases that start with same letter do:
+  first_order = arr.sort_by {|a| esp_alphabet.index(a[0])}
   
 end
