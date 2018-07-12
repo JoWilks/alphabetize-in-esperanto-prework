0374d627637463d67600201a717de1e14eb1541e
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-12 08:37:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
 
-  first_order = arr.sort_by {|a| esp_alphabet.index(a[0]) }
+  first_order = arr.sort_by {|a| esp_alphabet.index(a) }
   
-  if first_order #has any phrases that start with same letter do:
-  second_order = arr.sort_by {|a| esp_alphabet.index(a[1])}
+  #if first_order #has any phrases that start with same letter do:
+  #second_order = arr.sort_by {|a| esp_alphabet.index(a)}
   
 end
