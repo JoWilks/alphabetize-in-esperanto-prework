f5aaf1d1152b662ec78ac66b3fd159e030ab8574
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-12 08:35:21 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,8 @@
 def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
 
-  first_order = arr.sort_by {|a| esp_alphabet.index(a[0])}
+  first_order = arr.sort_by {|a| esp_alphabet.index(a[0]) 
+      esp_alphabet.index(a[1])
+  }
   
 end
