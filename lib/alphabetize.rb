c5f91499ef1584557919edc98d88b54929febae8
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-12 08:30:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,17 +1,3 @@
 def alphabetize(arr)
-  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
-  ordered = []
-  esp_alphabet.each_with_index {|letter, index| 
-    arr.each {|phrase| 
-      #split_word= word.split(//)
-      if phrase[0] == letter #&& ordered.empty?
-        ordered << phrase
-        #curr_ind = index
-      end
-    }
-  }
-  ordered
-    ordered.each {|phrase|
-        
-      }
+  puts arr
 end
